6d25d976cc9dcbe56186ac25141d927d81178305
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-08-24 16:40:26 -0300

    feat: Iniciando componete divider

---
+++ src/components/Divider/Divider.jsx
@@ -0,0 +1,10 @@
+import "./Divider.css"
+const Divider = () => {
+  return (
+    <section className="divider">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam cum voluptatum quo ad atque quasi in, enim delectus repudiandae sit consectetur vero. Qui aliquam incidunt autem deserunt totam nihil rerum.
+    </section>
+  )
+}
+
+export default Divider
--- src/pages/Client/HomePage.jsx
+++ src/pages/Client/HomePage.jsx
@@ -1,12 +1,19 @@
 import Header from "../../components/Header/Header"
+import LogoPrincipal from "../../assets/LogoPrincipal.png"
+import "./HomePage.css"
+import Divider from "../../components/Divider/Divider"
 
 const HomePage = () => {
   return (
     <>
     <Header/>
     <section className="hero">
-        
+        <div className="containerLogo">
+            <img src={LogoPrincipal} alt="logo principal"  className="imgPrincipal"/>
+            <p className="texto">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit labore quibusdam quis maiores autem veniam, possimus voluptate repellendus repudiandae similique nemo? Dolorum nostrum natus unde dolores in temporibus assumenda veritatis?</p>
+        </div>
     </section>
+    <Divider/>
     </>
   )
 }
